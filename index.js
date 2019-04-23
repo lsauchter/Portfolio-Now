0fd5b8facf6d3d6860876b21511e65ef4b5d32e8
Lauren Auchter <lsauchter@gmail.com>
2019-04-22 23:06:07 -0400

    Fixed nav menu bouncing bug - restructured JS

--- index.js
+++ index.js
@@ -1,55 +1,14 @@
-function startMenu() {
-    if ($(window).width() >= 650) {
-        $(".hambuger").hide();
-        $(".cross").hide();
-        $(".navlinks").show();
-    }
-    else {
-    $(".hambuger").show();
-    $(".navlinks").hide();
-    $(".cross").hide();
-    $(".hamburger").click(function() {
-        $(".navlinks").slideToggle( "slow", function() {
-            $(".hamburger").hide();
-            $(".cross").show();
+function menu() {
+    $(".navlinks").slideToggle( "slow", function() {
+            $("#hamburger").hide();
+            $("#cross").show();
             });
-        });
-    $(".cross").click(function() {
+        }
+
+function closeMenu() {
+        console.log("close ran");
         $( ".navlinks" ).slideToggle( "slow", function() {
-            $(".cross").hide();
-            $(".hamburger").show();
+            $("#cross").hide();
+            $("#hamburger").show();
             });
-         }); 
     }
-    handleResize();
-}
-
-function handleResize(){
-$(window).resize(function () {
-    if($(window).width() < 650) {
-        $(".hamburger").show();
-        $(".navlinks").hide();
-        $(".hamburger").click(function() {
-            $(".navlinks").slideToggle( "slow", function() {
-                $(".hamburger").hide();
-                $(".cross").show();
-                });
-            });
-        $(".cross").click(function() {
-            $( ".navlinks" ).slideToggle( "slow", function() {
-                $(".cross").hide();
-                $(".hamburger").show();
-                });
-             }); 
-    }
-    else {
-        $(".hambuger").hide();
-        $(".cross").hide();
-        $(".navlinks").show();
-    };
-})
-}
-
-$(document).ready(function() {
-        startMenu();
-    });
